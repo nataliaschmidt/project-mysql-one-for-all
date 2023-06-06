c132c3f6954e086a5f378029372a117780a9220a
nataliaschmidt <nataliak.schmidt@gmail.com>
2023-06-06 18:57:13 -0300

    feat: Requisito 02 -> query para exibir a quantidade total de cancoes, artistas e albuns

---
+++ challenges/desafio2.sql
@@ -0,0 +1,10 @@
+SELECT (
+    SELECT COUNT(cancao_id)
+    FROM cancoes
+) AS cancoes, (
+    SELECT COUNT(artista_id)
+    FROM artistas
+) AS artistas, (
+    SELECT COUNT(album_id)
+    FROM albuns
+) AS albuns;
