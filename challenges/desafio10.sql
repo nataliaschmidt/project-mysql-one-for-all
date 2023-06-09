501e1a84a11009d245ab33a3d13dcbcbdb422110
nataliaschmidt <nataliak.schmidt@gmail.com>
2023-06-09 14:50:09 -0300

    Fix: padronizando a tabela

--- challenges/desafio10.sql
+++ challenges/desafio10.sql
@@ -8,16 +8,17 @@ CREATE TABLE musicas_favoritas
     FOREIGN KEY (cancao_id) REFERENCES cancoes (cancao_id)
 );
 
-INSERT INTO musicas_favoritas (usuario_id, cancao_id) VALUES ('1', '3');
-INSERT INTO musicas_favoritas (usuario_id, cancao_id) VALUES ('1', '6');
-INSERT INTO musicas_favoritas (usuario_id, cancao_id) VALUES ('1', '10');
-INSERT INTO musicas_favoritas (usuario_id, cancao_id) VALUES ('2', '4');
-INSERT INTO musicas_favoritas (usuario_id, cancao_id) VALUES ('3', '1');
-INSERT INTO musicas_favoritas (usuario_id, cancao_id) VALUES ('3', '3');
-INSERT INTO musicas_favoritas (usuario_id, cancao_id) VALUES ('4', '7');
-INSERT INTO musicas_favoritas (usuario_id, cancao_id) VALUES ('4', '4');
-INSERT INTO musicas_favoritas (usuario_id, cancao_id) VALUES ('5', '10');
-INSERT INTO musicas_favoritas (usuario_id, cancao_id) VALUES ('5', '2');
-INSERT INTO musicas_favoritas (usuario_id, cancao_id) VALUES ('8', '4');
-INSERT INTO musicas_favoritas (usuario_id, cancao_id) VALUES ('9', '7');
-INSERT INTO musicas_favoritas (usuario_id, cancao_id) VALUES ('10', '3');
+INSERT INTO musicas_favoritas (usuario_id, cancao_id) VALUES
+(1, 3),
+(1, 6),
+(1, 10),
+(2, 4),
+(3, 1),
+(3, 3),
+(4, 7),
+(4, 4),
+(5, 10),
+(5, 2),
+(8, 4),
+(9, 7),
+(10, 3);
